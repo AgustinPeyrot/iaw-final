89dbaf4215a2a33f1e980ac9cad11b6a80579dd4
Agustin Peyrot <santiagopeyrot@gmail.com>
2024-01-11 10:11:10 -0300

    instalacion de todoist api y se agregan algunos compoenents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavbarComponent from './components/Navbar';
+import TabsExample from './components/ListGroups';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <TabsExample />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function NavbarComponent() {
   return (
     <Navbar className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+      <Navbar.Brand href="#home">DoItHub</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Tareas</Nav.Link>
+          <Nav.Link href="#features">Registro de tiempo</Nav.Link>
+        </Nav>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
+            Signed in as: <a href="#login">Agus Pey</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
+  <Outlet/>
 }
 
 export default NavbarComponent;
