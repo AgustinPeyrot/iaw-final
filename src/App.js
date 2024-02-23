5564c587bedac292457dd923368fcf0574468306
Agustin Peyrot <santiagopeyrot@gmail.com>
2024-02-22 21:01:48 -0300

    Se mueve la logica de taskCard.js a app.js
    este cambio permite que los componentes hijos compartan y actualicen su estado a través de las props del componente padre (app.js)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import NavbarComponent from './components/Navbar';
 import SidebarComponent from './components/Sidebar';
-import CardComponent from './components/TaskCard'
+import CardComponent from './components/TaskCard';
+import { getTareas, deleteTarea } from './routes/airtable';
 
 const AppContainer = styled.div`
   .split-screen {
@@ -24,13 +25,38 @@ const MainContentArea = styled.div`
 `;
 
 function App() {
+  const [tareas, setTareas] = useState([]);
+
+  const handleResolve = async (id) => {
+    try {
+      await deleteTarea(id);
+      const nuevasTareas = tareas.filter(tarea => tarea.id !== id);
+      setTareas(nuevasTareas);
+    } catch (error) {
+      console.error('Error al resolver la tarea:', error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchTareas = async () => {
+      try {
+        const tareasData = await getTareas();
+        setTareas(tareasData);
+      } catch (error) {
+        console.error('Error al obtener las tareas:', error);
+      }
+    };
+
+    fetchTareas();
+  }, []);
+
   return (
     <AppContainer className="App">
       <NavbarComponent />
       <div className="split-screen">
         <SidebarComponent />
         <MainContentArea>
-          <CardComponent />
+          <CardComponent tareas={tareas} handleResolve={handleResolve} />
         </MainContentArea>
       </div>
     </AppContainer>
--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { getTareas, deleteTarea } from '../routes/airtable';
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function TareasCards() {
-  const [tareas, setTareas] = useState([]);
-
-  useEffect(() => {
-    const fetchTareas = async () => {
-      try {
-        const tareasData = await getTareas();
-        setTareas(tareasData);
-      } catch (error) {
-        console.error('Error al obtener las tareas:', error);
-      }
-    };
-
-    fetchTareas();
-  }, []);
-
+function TareasCards({ tareas, handleResolve }) {
   const asignarColorPrioridad = (prioridad) => {
     switch (prioridad) {
       case 'Baja':
@@ -34,14 +17,6 @@ function TareasCards() {
       default:
         return 'light';
     }
-  };
-
-  const handleResolve = (id) => {
-    deleteTarea(id);
-    //filtrar las tareas para que no incluyan la tarea eliminada
-    const nuevasTareas = tareas.filter(tarea => tarea.id !== id);
-    //actualizar el estado con las tareas restantes
-    setTareas(nuevasTareas);
   };
 
   return (
